913d13842e34fc586e42aaa782f15eea4921f1d8
mukamif <163900626+mukamif@users.noreply.github.com>
2024-03-18 22:36:16 +0300

    Add files via upload

---
+++ control/src/main/java/com/mycompany/control/Control.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.control;
+
+/**
+ *
+ * @author Eddy
+ */
+public class Control {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
